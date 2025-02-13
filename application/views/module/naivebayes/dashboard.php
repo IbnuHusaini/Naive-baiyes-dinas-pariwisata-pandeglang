f8b3d5f148df43284f536535182bf46d620c3ddf
ibnu husaini <ibnuhusaini9@gmail.com>
2025-02-13 17:02:25 +0700

    Add files via upload

---
+++ application/views/module/naivebayes/dashboard.php
@@ -0,0 +1,7 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="card-box" align="center">
+          <h3>Prediksi Kunjungan Wisatawan Pada Dinas Pariwisata Kabupaten Pandeglang</h3>
+        </div>
+    </div>
+</div>
